39a93a0454bee37db1135b93687d6362910647ae
marirr86 <rr.mari@hotmail.com>
2021-07-23 19:27:41 -0300

    Update: CSS (nav, header, filterBar)

--- src/data.js
+++ src/data.js
@@ -1,7 +1,35 @@
-export const anotherExample = () => {
-  return 'OMG';
-};
 
-export const dataRM = {
+export const toOrder = (data, order) => {
+  
+  let showOrder = data.sort(function(a,z){
+    if(a.name < z.name) {
+      return order=="A-Z"? -1:1
+    }
+    if(a.name > z.name) {
+      return order=="A-Z"? 1:-1;
+    }
+  })
+  return showOrder
+  
 }
 
+/*export const toDisorder = (data) => {
+  let showDisorder = data.sort(function(a,z){
+    if(a.name < z.name) {
+      return 1;
+    }
+    if(a.name > z.name) {
+      return -1;
+    }
+  })
+  return showDisorder
+}*/
+
+
+/*export const sortData = (data, order) => {
+  if (order === "aToZ") {
+    return data.sort((a, z) => a.name > z.name ? 1 : -1)
+  } else {
+    return data.sort((a, z) => a.name > z.name ? -1 : 1)
+  }
+};*/
--- src/main.js
+++ src/main.js
@@ -1,86 +1,110 @@
-//import { computeStats, filterData, sortData, searchName } from "./data.js";
 
 import data from "./data/rickandmorty/rickandmorty.js";
+import { toOrder } from "./data.js";
 
-const dataBase = data.results;
 
 /*const home = document.getElementById("homeButton");
 const seasons = document.getElementById("seasonsButton");
 const episodes = document.getElementById("episodesButton");
 const characters = document.getElementById("charactersButton");
-
-
 const btn = document.getElementById("btn"); 
 const btnClear = document.getElementById("clearButton");
-const btnSort = document.getElementById("sortButton");
 const cards = document.querySelector("cards");
 const statusFilter = document.getElementById("statusFilter");
-const genderFilter = document.getElementById("genderFilter");
+const genderFilter = document.getElementById("genderFilter");*/
 
-const printTotalCharacters = document.getElementById("totalCharacters");
-const printGenderAverage = document.getElementById("genderAverage"); */
+//PRINTING CARDS FUNTION
 
-//PRINTING CARDS
+function showCards(data) {
 
-const const__container = document.getElementById("cardContainer");
-
-function showCards(filterData) {
-  const dataFrame = `
+  document.getElementById('cardContainer').innerHTML = data.map((item) => `
   <div class="container__cards__flex"> 
     <div class="card__normal"> 
-
-        <figure> <img class="card__figure" src="${filterData.image}">
-        </figure>
-
-        <div class="card__normal__texto">
-
-          <h3 class="characterName"> Name: ${filterData.name} </h3>
-
-          <div class="info">
-
-            <div class="left__info">
-              <p> Status: ${filterData.status}</p>
-              <p> Gender: ${filterData.gender}</p>
-              <p> Species: ${filterData.species}</p>
-            </div>
-            <div class="right__info">
-              <p> Type: ${filterData.type}</p>
-              <p> Origin: ${filterData.origin.name}</p>
-              <p> Location: ${filterData.location.name}</p>
-            </div>
-
-          </div>
-
-        </div>
-    </div> 
-  </div> 
-  `;
-  return dataFrame;
+      <figure> <img class="card__figure" src="${item.image}">
+      </figure>
+      <div class="card__normal__texto">
+        <h3 class="characterName">${item.name}</h3>
+      </div>
+    <div class="info">
+      <div class="left__info">
+        <ul class="list">
+          <li class="list__item">Status: ${item.status}</li>
+          <li class="list__item">Gender: ${item.gender}</li>
+          <li class="list__item">Species: ${item.species}</li>
+          <li class="list__item">Appears in: ${item.episode.length} episodes</li>
+          <li class="list__item">Origin: ${item.origin.name}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+        
+ `).join("");
 }
 
-let allCards = "";
+showCards(data.results);
 
-for (let index = 0; index < dataBase.length; index++) {
-  const filterData = dataBase[index];
-  allCards += showCards(filterData);
+
+//ORDERING FUNCTION: A to Z
+
+const order = document.getElementById("fromAtoZ");
+
+function getOrderData() {
+
+  showCards(toOrder(data.results, "A-Z"));
 }
+order.addEventListener("click", () => { getOrderData() });
 
-const__container.inmentnerHTML = allCards;
+
+//ORDERING FUNCTION: Z to A 
+
+const disorder = document.getElementById("fromZtoA");
+
+function getDisorderData() {
+
+  showCards(toOrder(data.results, "Z-A"));
+}
+disorder.addEventListener("click", () => { getDisorderData() });
+
+
+
+/* FUNCTION FILTER BY STATUS:
+
+
+function filter(e) {
+  e.preventDefault();
+  const statusOptions = statusFilter.options[statusFilter.selectedIndex].value;
+  const genderOptions = genderFilter.options[genderFilter.selectedIndex].value;
+  const filterValue = filterData(data.results, statusOptions, genderOptions);
+  printCardsGeneric(filterValue);
+}
+btn.addEventListener("click", filter);*/
+
+
+
+
+/*function getOrderData() {
+  showCards(getOrder(data.results));
+}
+order.addEventListener("click", () => { getOrderData() });*/
+
+
+
+
+
+//sortCards = document.getElementById("filter")
+//btnSort.addEventListener("click", sort);
 
 
 
 //Cálculos estatísticos:
 
 /*const totalCharacters = computeStats.characters(data.results);
-
 printTotalCharacters.innerHTML = `<p class="totalCharacter">O total de personagens da série é:</p>
                                   <p class="numberOfCharacters">${totalCharacters}</p>`
-
 const maleAverage = computeStats.gender(data.results, "Male") + "%";
 const femaleAverage = computeStats.gender(data.results, "Female") + "%";
 const genderlessAverage = computeStats.gender(data.results, "Genderless") + "%";
 const unknownAverage = computeStats.gender(data.results, "unknown") + "%";
-
 printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
                                 <p class="genderAverage">Masculinos:
                                   <span>${maleAverage}</span>
@@ -94,42 +118,15 @@ printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
                                 <p class="genderAverage">Sem gênero:
                                   <span>${genderlessAverage}</span>
                                 </p>`
-
-// Função filtrar:    
-
-function filter(e) {
-  e.preventDefault();
-  const statusOptions = statusFilter.options[statusFilter.selectedIndex].value;
-  const genderOptions = genderFilter.options[genderFilter.selectedIndex].value;
-  const filterValue = filterData(data.results, statusOptions, genderOptions);
-  printCardsGeneric(filterValue);
-}
-btn.addEventListener("click", filter);
-
-
-// Função ordenar:
-
-function sort(e) {
-  e.preventDefault();
-  const sortCards = sortData(data.results);
-  printCardsGeneric(sortCards);
-}
-btnSort.addEventListener("click", sort);
-
-
 //Botão limpar:
-
-function clearFilters(e) {
+/*function clearFilters(e) {
   e.preventDefault()
   printCardsGeneric(data.results);
   statusFilter.options[statusFilter.selectedIndex = 0];
   genderFilter.options[genderFilter.selectedIndex = 0];
 }
 btnClear.addEventListener("click", clearFilters);
-
-
 // Pesquisar por nome do personagem:
-
 function searchByName (e){
   const charactersByName = searchName(data.results, e.target.value);
   printCardsGeneric(charactersByName);
@@ -138,10 +135,8 @@ searchInput.addEventListener("keyup", searchByName);
 export const example = () => {
   return 'example';
 };
-
 export const anotherExample = () => {
   return 'OMG';
 };
-
 export const dataRM = {
 } */
