2a16e1334ef3ec1b3f9a5bcbc0e38a8b230fd5c9
marirr86 <rr.mari@hotmail.com>
2021-07-26 18:48:24 -0300

    Update: main, data, html e CSS

--- src/data.js
+++ src/data.js
@@ -13,23 +13,14 @@ export const toOrder = (data, order) => {
   
 }
 
-/*export const toDisorder = (data) => {
-  let showDisorder = data.sort(function(a,z){
-    if(a.name < z.name) {
-      return 1;
-    }
-    if(a.name > z.name) {
-      return -1;
-    }
-  })
-  return showDisorder
-}*/
+export const getStatus = (data, targetValue) => {
 
+  return data.filter((item) => item.status === targetValue );
+  
+  } 
 
-/*export const sortData = (data, order) => {
-  if (order === "aToZ") {
-    return data.sort((a, z) => a.name > z.name ? 1 : -1)
-  } else {
-    return data.sort((a, z) => a.name > z.name ? -1 : 1)
-  }
-};*/
+export const getGender = (data, targetValue) => {
+
+  return data.filter((item) => item.gender === targetValue );
+    
+  } 
--- src/main.js
+++ src/main.js
@@ -1,32 +1,26 @@
 
 import data from "./data/rickandmorty/rickandmorty.js";
-import { toOrder } from "./data.js";
+import { toOrder, getStatus, getGender} from "./data.js";
 
 
-/*const home = document.getElementById("homeButton");
-const seasons = document.getElementById("seasonsButton");
-const episodes = document.getElementById("episodesButton");
-const characters = document.getElementById("charactersButton");
-const btn = document.getElementById("btn"); 
-const btnClear = document.getElementById("clearButton");
-const cards = document.querySelector("cards");
-const statusFilter = document.getElementById("statusFilter");
-const genderFilter = document.getElementById("genderFilter");*/
+const order = document.getElementById("fromAtoZ");
+const disorder = document.getElementById("fromZtoA");
+const statusDOM = document.getElementById("statusFilter");
+const genderDOM = document.getElementById("genderFilter");
+const clearBtn = document.getElementById("clearButton")
 
 //PRINTING CARDS FUNTION
 
 function showCards(data) {
 
   document.getElementById('cardContainer').innerHTML = data.map((item) => `
-  <div class="container__cards__flex"> 
     <div class="card__normal"> 
       <figure> <img class="card__figure" src="${item.image}">
       </figure>
       <div class="card__normal__texto">
         <h3 class="characterName">${item.name}</h3>
       </div>
-    <div class="info">
-      <div class="left__info">
+      <div class="info">
         <ul class="list">
           <li class="list__item">Status: ${item.status}</li>
           <li class="list__item">Gender: ${item.gender}</li>
@@ -36,17 +30,13 @@ function showCards(data) {
         </ul>
       </div>
     </div>
-  </div>
         
  `).join("");
 }
 
 showCards(data.results);
 
-
 //ORDERING FUNCTION: A to Z
-
-const order = document.getElementById("fromAtoZ");
 
 function getOrderData() {
 
@@ -54,10 +44,7 @@ function getOrderData() {
 }
 order.addEventListener("click", () => { getOrderData() });
 
-
 //ORDERING FUNCTION: Z to A 
-
-const disorder = document.getElementById("fromZtoA");
 
 function getDisorderData() {
 
@@ -65,78 +52,36 @@ function getDisorderData() {
 }
 disorder.addEventListener("click", () => { getDisorderData() });
 
+// FUNCTION FILTER BY STATUS:
 
+function target(option) {
 
-/* FUNCTION FILTER BY STATUS:
-
-
-function filter(e) {
-  e.preventDefault();
-  const statusOptions = statusFilter.options[statusFilter.selectedIndex].value;
-  const genderOptions = genderFilter.options[genderFilter.selectedIndex].value;
-  const filterValue = filterData(data.results, statusOptions, genderOptions);
-  printCardsGeneric(filterValue);
+  let targetValue = option.target.value;
+  let result = getStatus(data.results, targetValue);
+  document.getElementById('cardContainer').innerHTML = ""
+  showCards(result)
 }
-btn.addEventListener("click", filter);*/
 
+statusDOM.addEventListener("change", (option) => {target(option)});
 
+// FUNCTION FILTER BY GENDER:
 
+function gender2(option) {
 
-/*function getOrderData() {
-  showCards(getOrder(data.results));
+  let targetValue = option.target.value;
+  let result = getGender(data.results, targetValue);
+  document.getElementById('cardContainer').innerHTML = ""
+  showCards(result)
 }
-order.addEventListener("click", () => { getOrderData() });*/
 
+genderDOM.addEventListener("change", (option) => {gender2(option)});
 
+// FUNCTION CLEAR:
 
-
-
-//sortCards = document.getElementById("filter")
-//btnSort.addEventListener("click", sort);
-
-
-
-//Cálculos estatísticos:
-
-/*const totalCharacters = computeStats.characters(data.results);
-printTotalCharacters.innerHTML = `<p class="totalCharacter">O total de personagens da série é:</p>
-                                  <p class="numberOfCharacters">${totalCharacters}</p>`
-const maleAverage = computeStats.gender(data.results, "Male") + "%";
-const femaleAverage = computeStats.gender(data.results, "Female") + "%";
-const genderlessAverage = computeStats.gender(data.results, "Genderless") + "%";
-const unknownAverage = computeStats.gender(data.results, "unknown") + "%";
-printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
-                                <p class="genderAverage">Masculinos:
-                                  <span>${maleAverage}</span>
-                                </p>
-                                <p class="genderAverage">Femininos:
-                                  <span>${femaleAverage}</span>
-                                </p>
-                                <p class="genderAverage">Desconhecidos:
-                                  <span>${unknownAverage}</span>
-                                </p>
-                                <p class="genderAverage">Sem gênero:
-                                  <span>${genderlessAverage}</span>
-                                </p>`
-//Botão limpar:
-/*function clearFilters(e) {
+function clear(e) {
   e.preventDefault()
-  printCardsGeneric(data.results);
-  statusFilter.options[statusFilter.selectedIndex = 0];
-  genderFilter.options[genderFilter.selectedIndex = 0];
-}
-btnClear.addEventListener("click", clearFilters);
-// Pesquisar por nome do personagem:
-function searchByName (e){
-  const charactersByName = searchName(data.results, e.target.value);
-  printCardsGeneric(charactersByName);
-}
-searchInput.addEventListener("keyup", searchByName);
-export const example = () => {
-  return 'example';
-};
-export const anotherExample = () => {
-  return 'OMG';
-};
-export const dataRM = {
-} */
+  showCards(data.results);
+  statusDOM.options[statusDOM.selectedIndex = 0];
+  genderDOM.options[genderDOM.selectedIndex = 0];
+  }
+  clearBtn.addEventListener("click", clear); 
