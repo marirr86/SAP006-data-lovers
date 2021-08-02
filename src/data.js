d475beb36645be71f05e7903682bbb56cae0aca2
Rafael Cerri <rafaelbcerri@gmail.com>
2021-08-02 18:00:39 -0300

    aa

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const ordering = (arr, e) => {
   const element = [...arr];
-  if (e === "a-z") {
+  if (e === "A-Z") {
       return element.sort((a, b) => a.name > b.name ? 1 : -1);
   } else if (e === "z-a") {
       return element.sort((a, b) => a.name > b.name ? -1 : 1);
@@ -9,15 +9,15 @@ export const ordering = (arr, e) => {
 
 export const getSpecies = (data, target) => {
   return data.filter((item) => item.species === target );
-      
-  } 
+
+  }
 
 export const getStatus = (data, target) => {
   return data.filter((item) => item.status === target );
-  
-  } 
+
+  }
 
 export const getGender = (data, target) => {
   return data.filter((item) => item.gender === target );
-    
-  } 
+
+  }
